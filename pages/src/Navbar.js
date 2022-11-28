6d494e8acb82187ee2e32b1362ad25d23a798e67
natdanai mos <dev@dosetech.co>
2022-11-28 14:22:09 +0700

    fixed bug

--- pages/src/Navbar.js
+++ pages/src/Navbar.js
@@ -5,7 +5,6 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import Link from "next/link";
 
 const useStyles = makeStyles((theme) => ({
    root: {
